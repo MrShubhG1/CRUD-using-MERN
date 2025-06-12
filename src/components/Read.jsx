8ce41da2d3454ce83cd5a775318407a89bfa9930
Shubham Gupta <codewithmr.shubh@gmail.com>
2025-06-12 11:40:21 +0530

    bug fix

--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -6,7 +6,16 @@ const Read = () => {
   const [error, setError] = useState("");
   const [selectedID, setSelectedId] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("")
+  const [searchTerm, setSearchTerm] = useState("");
+  const selectOption = [
+    { id: 1, name: "Name" },
+    { id: 2, name: "Email" },
+    { id: 3, name: "Age" },
+  ];
+  const [selectedOption, setSelectedOption] = useState("Filter");
+  const handelclick = (option) => {
+    setSelectedOption(option);
+  };
   const openModal = (id) => {
     setSelectedId(id);
   };
@@ -62,12 +71,14 @@ const Read = () => {
       <h2 className="text-center h4">All Users</h2>
       <div className="row">
         {loading ? (
-                 <div
+          <div
             className="col-12 col-sm-6 col-lg-4  me-2 mb-4"
-            
             aria-hidden="true"
           >
-            <div className="card-body rounded shadow-lg d-flex flex-column justify-content-between  " style={{height:"200px"}}>
+            <div
+              className="card-body rounded shadow-lg d-flex flex-column justify-content-between  "
+              style={{ height: "200px" }}
+            >
               <h5 className="card-title placeholder-glow pt-3">
                 <span className="placeholder col-6"></span>
               </h5>
@@ -86,61 +97,108 @@ const Read = () => {
           </div>
         ) : data.length === 0 ? (
           <div className="text-muted text-center mt-3">No User Found</div>
-        ) : ( <>
-          
+        ) : (
+          <>
             <div className="row justify-content-center mt-2 mb-2">
-              <div className="col-lg-5">
+              <div className="col-lg-6">
                 <div className="input-group">
+                  {/* Dropdown on left */}
+                  <div className="input-group-prepend">
+                    <button
+                      className="btn btn-secondary py-3 dropdown-toggle rounded-start-pill"
+                      type="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      {selectedOption}
+                    </button>
+                    <ul className="dropdown-menu">
+                      {selectOption.map((elem) => (
+                        <li>
+                          <button
+                            className="dropdown-item"
+                            key={elem.id}
+                            value={elem.name}
+                            onClick={() => handelclick(elem.name)}
+                          >
+                            {elem.name}
+                          </button>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+
+                  {/* Input box on right */}
                   <input
                     type="text"
-                    className="form-control rounded-pill px-3 py-2 shadow-sm "
+                    className="form-control rounded-end-pill px-3 py-2 shadow-sm"
                     placeholder="Search..."
-                    value = {searchTerm}
-                    onChange= {(e)=>setSearchTerm(e.target.value)}
-                    style={{outline:"none", boxShadow:"none", border:"0px",}}
-                  ></input>
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    style={{
+                      outline: "none",
+                      boxShadow: "none",
+                      border: "0px",
+                    }}
+                  />
                 </div>
               </div>
             </div>
-           
-          {data.filter((elem)=>(
-             elem.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-             elem.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
-             elem.age.toString().includes(searchTerm) 
-          ))
-          .map((elem) => (
-            <div
-              key={elem._id}
-              className="row col-10 col-sm-6 col-lg-4 me-2 mb-4"
-            >
-              <div
-                className="card d-flex flex-column justify-content-between shadow p-3 mt-2 bg-white border-0 rounded  "
-                style={{ height: "200px" }}
-              >
-                <div className="card-body">
-                  <h5>Name : {elem.name}</h5>
-                  <h6 className="card-subtitle mb-2 text-body-secondary">
-                    Email : {elem.email}
-                  </h6>
-                  <p className="card-text">Age : {elem.age}</p>
-                </div>
-                <div className="d-flex gap-3 justify-content-end">
-                  <Link to={`/${elem._id}`} className="btn btn-primary px-4">
-                    Edit
-                  </Link>
-                  <Link
-                    type="button"
-                    className="btn btn-danger"
-                    data-bs-toggle="modal"
-                    data-bs-target="#exampleModal"
-                    onClick={() => openModal(elem._id)}
+
+            {data
+              .filter((elem) => {
+                if (selectedOption === "Name") {
+                  return elem.name
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase());
+                }
+                if (selectedOption === "Email") {
+                  return elem.email
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase());
+                }
+                if (selectedOption === "Age") {
+                  return elem.age.toString().includes(searchTerm);
+                }
+                return true;
+              })
+              .map((elem) => (
+                <div
+                  key={elem._id}
+                  className="row col-10 col-sm-6 col-lg-4 me-2 mb-4"
+                >
+                  <div
+                    className="card d-flex flex-column justify-content-between shadow p-3 mt-2 bg-white border-0 rounded  "
+                    style={{ height: "200px" }}
                   >
-                    Delete
-                  </Link>
+                    <div className="card-body">
+                      <h5>Name : {elem.name}</h5>
+                      <h6 className="card-subtitle mb-2 text-body-secondary">
+                        Email : {elem.email}
+                      </h6>
+                      <p className="card-text">Age : {elem.age}</p>
+                    </div>
+                    <div className="d-flex gap-3 justify-content-end">
+                      <Link
+                        to={`/${elem._id}`}
+                        className="btn btn-primary px-4"
+                      >
+                        Edit
+                      </Link>
+                      <Link
+                        type="button"
+                        className="btn btn-danger"
+                        data-bs-toggle="modal"
+                        data-bs-target="#exampleModal"
+                        onClick={() => openModal(elem._id)}
+                      >
+                        Delete
+                      </Link>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          ))}</> 
+              ))}
+          </>
         )}
       </div>
 
